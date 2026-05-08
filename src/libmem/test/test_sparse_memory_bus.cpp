35c75b2b69ee903739e6e0e4e109705a1d5d3a3b
Your Name <you@example.com>
2026-05-07 22:24:39 -0400

    Unit tests for SparseMemory and MapMmu

--- src/libmem/test/test_sparse_memory_bus.cpp
+++ src/libmem/test/test_sparse_memory_bus.cpp
@@ -108,3 +108,152 @@ TEST_CASE(sparsemembus_reset) {
     ASSERT(bus.read8(0x080000) == 0xAA);
     ASSERT(bus.writeCount() == 0);
 }
+
+TEST_CASE(sparsemembus_page_boundaries) {
+    SparseMemoryBus bus("test", 28);
+
+    bus.write8(0x000FFF, 0x11);
+    bus.write8(0x001000, 0x22);
+    bus.write8(0x001001, 0x33);
+
+    ASSERT(bus.read8(0x000FFF) == 0x11);
+    ASSERT(bus.read8(0x001000) == 0x22);
+    ASSERT(bus.read8(0x001001) == 0x33);
+}
+
+TEST_CASE(sparsemembus_address_masking) {
+    SparseMemoryBus bus("test", 16);
+
+    bus.write8(0x002000, 0x55);
+    ASSERT(bus.read8(0x002000) == 0x55);
+    ASSERT(bus.read8(0x012000) == 0x55);
+}
+
+TEST_CASE(sparsemembus_region_precedence) {
+    SparseMemoryBus bus("test", 28);
+    uint8_t rom1[] = {0xAA, 0xBB, 0xCC, 0xDD};
+    uint8_t rom2[] = {0x11, 0x22};
+
+    bus.addRegion(0x100000, 4, rom1, false);
+    bus.addRegion(0x100002, 2, rom2, false);
+
+    ASSERT(bus.read8(0x100000) == 0xAA);
+    ASSERT(bus.read8(0x100001) == 0xBB);
+    ASSERT(bus.read8(0x100002) == 0xCC);
+    ASSERT(bus.read8(0x100003) == 0xDD);
+}
+
+TEST_CASE(sparsemembus_writable_region) {
+    SparseMemoryBus bus("test", 28);
+    uint8_t data[] = {0x10, 0x20, 0x30, 0x40};
+
+    bus.addRegion(0x050000, 4, data, true);
+
+    ASSERT(bus.read8(0x050000) == 0x10);
+    bus.write8(0x050001, 0xAA);
+    ASSERT(bus.read8(0x050001) == 0xAA);
+}
+
+TEST_CASE(sparsemembus_rom_write_ignored) {
+    SparseMemoryBus bus("test", 28);
+    uint8_t rom[] = {0x55};
+
+    bus.addRegion(0x060000, 1, rom, false);
+
+    ASSERT(bus.read8(0x060000) == 0x55);
+    bus.write8(0x060000, 0xFF);
+    ASSERT(bus.read8(0x060000) == 0x55);
+}
+
+TEST_CASE(sparsemembus_write_log_max) {
+    SparseMemoryBus bus("test", 28);
+    bus.clearWriteLog();
+
+    for (int i = 0; i < 10; ++i) {
+        bus.write8(0x001000 + i, 0x10 + i);
+    }
+
+    uint32_t addrs[5];
+    uint8_t before[5];
+    uint8_t after[5];
+    bus.getWrites(addrs, before, after, 5);
+
+    ASSERT(addrs[0] == 0x001000);
+    ASSERT(after[0] == 0x10);
+    ASSERT(addrs[4] == 0x001004);
+}
+
+TEST_CASE(sparsemembus_sequential_writes) {
+    SparseMemoryBus bus("test", 28);
+    bus.clearWriteLog();
+
+    bus.write8(0x001000, 0xFF);
+    bus.write8(0x001000, 0x00);
+    bus.write8(0x001000, 0xAA);
+
+    ASSERT(bus.writeCount() == 3);
+    ASSERT(bus.read8(0x001000) == 0xAA);
+
+    uint32_t addrs[3];
+    uint8_t before[3];
+    uint8_t after[3];
+    bus.getWrites(addrs, before, after, 3);
+
+    ASSERT(before[0] == 0xFF);
+    ASSERT(after[0] == 0xFF);
+    ASSERT(before[1] == 0xFF);
+    ASSERT(after[1] == 0x00);
+    ASSERT(before[2] == 0x00);
+    ASSERT(after[2] == 0xAA);
+}
+
+TEST_CASE(sparsemembus_peek_no_log) {
+    SparseMemoryBus bus("test", 28);
+    bus.clearWriteLog();
+
+    bus.write8(0x001000, 0x42);
+    ASSERT(bus.writeCount() == 1);
+
+    bus.peek8(0x001000);
+    ASSERT(bus.writeCount() == 1);
+}
+
+TEST_CASE(sparsemembus_state_multiple_pages) {
+    SparseMemoryBus bus("test", 28);
+
+    bus.write8(0x001000, 0xAA);
+    bus.write8(0x002000, 0xBB);
+    bus.write8(0x003000, 0xCC);
+
+    size_t size = bus.stateSize();
+    std::vector<uint8_t> buffer(size);
+    bus.saveState(buffer.data());
+
+    bus.write8(0x001000, 0x00);
+    bus.write8(0x002000, 0x00);
+    bus.write8(0x003000, 0x00);
+
+    bus.loadState(buffer.data());
+    ASSERT(bus.read8(0x001000) == 0xAA);
+    ASSERT(bus.read8(0x002000) == 0xBB);
+    ASSERT(bus.read8(0x003000) == 0xCC);
+}
+
+TEST_CASE(sparsemembus_region_at_zero) {
+    SparseMemoryBus bus("test", 28);
+    uint8_t data[] = {0x00, 0x01, 0x02};
+
+    bus.addRegion(0x000000, 3, data, false);
+
+    ASSERT(bus.read8(0x000000) == 0x00);
+    ASSERT(bus.read8(0x000001) == 0x01);
+    ASSERT(bus.read8(0x000002) == 0x02);
+}
+
+TEST_CASE(sparsemembus_large_address) {
+    SparseMemoryBus bus("test", 28);
+
+    bus.write8(0x0FFFFFF, 0x77);
+    ASSERT(bus.read8(0x0FFFFFF) == 0x77);
+    ASSERT(bus.read8(0x0FFFFFE) == 0xFF);
+}
--- src/plugins/devices/map_mmu/test/test_map_mmu.cpp
+++ src/plugins/devices/map_mmu/test/test_map_mmu.cpp
@@ -84,3 +84,201 @@ TEST_CASE(map_mmu_reset) {
         ASSERT(mmu.getMapState().offsets[i] == 0);
     }
 }
+
+TEST_CASE(map_mmu_all_blocks_mapped) {
+    SparseMemoryBus physBus("phys", 28);
+    MapMmu mmu("mmu", &physBus);
+
+    for (int i = 0; i < 8; ++i) {
+        uint32_t phys = (0x1000 + i) << 8;
+        physBus.write8(phys, 0x10 + i);
+    }
+
+    MapState state = {};
+    for (int i = 0; i < 8; ++i) {
+        state.offsets[i] = 0x1000 + i;
+        state.enables |= (1 << i);
+    }
+    mmu.setMapState(state);
+
+    for (int i = 0; i < 8; ++i) {
+        uint32_t vaddr = i << 13;
+        ASSERT(mmu.read8(vaddr) == 0x10 + i);
+    }
+}
+
+TEST_CASE(map_mmu_block_0_lower_half) {
+    SparseMemoryBus physBus("phys", 28);
+    MapMmu mmu("mmu", &physBus);
+
+    physBus.write8(0x200000, 0xBB);
+    physBus.write8(0x200123, 0xCC);
+
+    MapState state = {};
+    state.offsets[0] = 0x2000;
+    state.enables = (1 << 0);
+    mmu.setMapState(state);
+
+    ASSERT(mmu.read8(0x0000) == 0xBB);
+    ASSERT(mmu.read8(0x0123) == 0xCC);
+}
+
+TEST_CASE(map_mmu_block_7_upper_half) {
+    SparseMemoryBus physBus("phys", 28);
+    MapMmu mmu("mmu", &physBus);
+
+    physBus.write8(0x500000, 0xDD);
+
+    MapState state = {};
+    state.offsets[7] = 0x5000;
+    state.enables = (1 << 7);
+    mmu.setMapState(state);
+
+    ASSERT(mmu.read8(0xE000) == 0xDD);
+}
+
+TEST_CASE(map_mmu_write_through_mapping) {
+    SparseMemoryBus physBus("phys", 28);
+    MapMmu mmu("mmu", &physBus);
+
+    MapState state = {};
+    state.offsets[4] = 0x300;
+    state.enables = (1 << 4);
+    mmu.setMapState(state);
+
+    mmu.write8(0x8000, 0x99);
+    ASSERT(physBus.read8(0x30000) == 0x99);
+    ASSERT(mmu.read8(0x8000) == 0x99);
+}
+
+TEST_CASE(map_mmu_partial_mapping) {
+    SparseMemoryBus physBus("phys", 28);
+    MapMmu mmu("mmu", &physBus);
+
+    physBus.write8(0x100000, 0x11);
+    physBus.write8(0x8000, 0x22);
+
+    MapState state = {};
+    state.offsets[2] = 0x1000;
+    state.enables = (1 << 2);
+    mmu.setMapState(state);
+
+    ASSERT(mmu.read8(0x4000) == 0x11);
+    ASSERT(mmu.read8(0x8000) == 0x22);
+}
+
+TEST_CASE(map_mmu_offset_masking) {
+    SparseMemoryBus physBus("phys", 28);
+    MapMmu mmu("mmu", &physBus);
+
+    uint32_t offset = 0xFFFFF;
+    uint32_t phys = (offset << 8) | (0x6000 & 0x1FFF);
+    physBus.write8(phys, 0xEE);
+
+    MapState state = {};
+    state.offsets[3] = 0xFFFFF;
+    state.enables = (1 << 3);
+    mmu.setMapState(state);
+
+    ASSERT(mmu.read8(0x6000) == 0xEE);
+}
+
+TEST_CASE(map_mmu_state_save_load) {
+    SparseMemoryBus physBus("phys", 28);
+    MapMmu mmu("mmu", &physBus);
+
+    MapState original = {};
+    original.offsets[1] = 0x111;
+    original.offsets[3] = 0x333;
+    original.offsets[5] = 0x555;
+    original.enables = (1 << 1) | (1 << 3) | (1 << 5);
+    mmu.setMapState(original);
+
+    size_t size = mmu.stateSize();
+    std::vector<uint8_t> buffer(size);
+    mmu.saveState(buffer.data());
+
+    MapState reset = {};
+    mmu.setMapState(reset);
+    ASSERT(mmu.getMapState().enables == 0);
+
+    mmu.loadState(buffer.data());
+    ASSERT(mmu.getMapState().enables == original.enables);
+    ASSERT(mmu.getMapState().offsets[1] == original.offsets[1]);
+    ASSERT(mmu.getMapState().offsets[3] == original.offsets[3]);
+    ASSERT(mmu.getMapState().offsets[5] == original.offsets[5]);
+}
+
+TEST_CASE(map_mmu_address_boundaries) {
+    SparseMemoryBus physBus("phys", 28);
+    MapMmu mmu("mmu", &physBus);
+
+    physBus.write8(0x200000, 0x01);
+    physBus.write8(0x201FFF, 0x02);
+
+    MapState state = {};
+    state.offsets[0] = 0x2000;
+    state.enables = (1 << 0);
+    mmu.setMapState(state);
+
+    ASSERT(mmu.read8(0x0000) == 0x01);
+    ASSERT(mmu.read8(0x1FFF) == 0x02);
+}
+
+TEST_CASE(map_mmu_block_transitions) {
+    SparseMemoryBus physBus("phys", 28);
+    MapMmu mmu("mmu", &physBus);
+
+    physBus.write8(0x300000, 0xA0);
+    physBus.write8(0x320000, 0xA1);
+
+    MapState state = {};
+    state.offsets[1] = 0x3000;
+    state.offsets[2] = 0x3200;
+    state.enables = (1 << 1) | (1 << 2);
+    mmu.setMapState(state);
+
+    ASSERT(mmu.read8(0x2000) == 0xA0);
+    ASSERT(mmu.read8(0x4000) == 0xA1);
+}
+
+TEST_CASE(map_mmu_peek_no_observers) {
+    SparseMemoryBus physBus("phys", 28);
+    MapMmu mmu("mmu", &physBus);
+
+    physBus.write8(0x100000, 0x55);
+
+    MapState state = {};
+    state.offsets[5] = 0x1000;
+    state.enables = (1 << 5);
+    mmu.setMapState(state);
+
+    uint8_t val = mmu.peek8(0xA000);
+    ASSERT(val == 0x55);
+}
+
+TEST_CASE(map_mmu_mixed_enabled_disabled) {
+    SparseMemoryBus physBus("phys", 28);
+    MapMmu mmu("mmu", &physBus);
+
+    physBus.write8(0x100000, 0x10);
+    physBus.write8(0x004000, 0x40);
+
+    MapState state = {};
+    state.offsets[2] = 0x1000;
+    state.enables = (1 << 2);
+    mmu.setMapState(state);
+
+    ASSERT(mmu.read8(0x4000) == 0x10);
+    ASSERT(mmu.read8(0x6000) == 0xFF);
+}
+
+TEST_CASE(map_mmu_config_validity) {
+    SparseMemoryBus physBus("phys", 28);
+    MapMmu mmu("mmu", &physBus);
+
+    const BusConfig& config = mmu.config();
+    ASSERT(config.addrBits == 16);
+    ASSERT(config.dataBits == 8);
+    ASSERT(config.addrMask == 0xFFFF);
+}
